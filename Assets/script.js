06ede82b9546154cf130f2544dd6898a3ed253b6
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-05-04 14:41:48 -0700

    gets current days weather

--- Assets/script.js
+++ Assets/script.js
@@ -2,37 +2,48 @@ $(document).ready(function() {
 
     function displayForecast() {
      
-        $("button").on("click", function() {
+        $("#search").on("click", function() {
 
-        var City = $(this).attr("{city ID}");
-        var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + City + "&appid=9508f5887b64149ae87a4e8e95cc981b";
+        var City = $("#citySearched").val();
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&appid=9508f5887b64149ae87a4e8e95cc981b";
 
         $.ajax({
         url: queryURL,
         method: "GET"
         }).then(function(response) {
 
-            var results = response.data;
-            for (var i = 0; i < results.length; i++) {
+            var results = response;
 
-            var city = $("results").attr("src", results[i].city.name);
+        //    var uvIndex = $("<span>");
+        //    uvIndex.text("UV Index: " + );
+        //    $("#results").append(uvIndex);
+        
+        var wind = $("<span>");
+        wind.text("Wind Speed: " + results.wind.speed);
+        $("#results").prepend(wind);
 
-            var date =$("results").attr("src", results[i].list.dt_txt);
+        var humidity = $("<span>");
+        humidity.text("Humidity: " + results.main.humidity);
+        $("#results").prepend(humidity);
+        
+        // put results in f
+        var temp = $("<span>");
+        temp.text("Temperature: " + results.main.temp);
+        $("#results").prepend(temp);
+         
+        var city = $("<span>");
+        city.text(results.name);
+        $("#results").prepend(city);
 
-            // put results in f
-           var temp = $("results").attr("src", results[i].list.main.temp_kf);
-
-           var humidity = $("results").attr("src", results[i].list.main.humidity);
-
-           var windSpeed = $("results").attr("src", results[i].list.wind.speed);
-
-           var uvIndex = $("results").attr("src", results[i].city);
-
-           var forecast = $("fiveDay").attr("src", results[i] );
-            }
+        // add todays date
 
         });
+
+
+            // var forecast = $("fiveDay").attr("src", results[i] );
+
 
         });
     }
+    displayForecast()
 });
