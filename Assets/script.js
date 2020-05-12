0aae5fd4f3cc4d3b631d2bbfb3d59d9fbcfa9542
Kaelyn Rieffer <kaelynrieffer@gmail.com>
2020-05-12 13:43:58 -0700

    added comments to code

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,33 @@
 $(document).ready(function () {
 
+    // stores current list of cities
     var currentList = [];
 
+    // list stores local storage, store makes it into an object 
     var store = JSON.parse(localStorage.getItem("list"));
 
+    // For every item in local storage make it a button
     if (store) {
         for (let i = 0; i < store.length; i++) {
 
             var cityList = $("<button>");
             cityList.text(store[i]);
             $(".cityAdded").append(cityList);
-
+            
+            // When the button is clicked on give the weather reports
             cityList.on("click", function (event) {
                 var btnText = $(event.target).text();
                 currentWeather(btnText);
                 fiveDayForecast(btnText);
             });
         }
+        // Current list includes what is in local storage, no repeate buttons.
         currentList = store.concat(currentList);
     }
 
     function displayForecast() {
 
+        // When the search button is clicked adds search to the list and gives the weather. 
         $(".searchBtn").on("click", function () {
             var City = $("#citySearched").val();
 
@@ -34,9 +40,12 @@ $(document).ready(function () {
 
     function currentWeather(City) {
 
+        // Clears the search results when the page is refreashed.
         $("#results").text("");
+        // Keeps the header.
         $("#forecast").html("<h2>5-Day Forecast:</h2>");
 
+        // Check if what was searched is already on the page, if not add it.
         if (!currentList.includes(City) && City !== "") {
             currentList.push(City);
 
@@ -44,17 +53,21 @@ $(document).ready(function () {
             cityList.text(City);
             $(".cityAdded").append(cityList);
 
+            // Give the weather when one of the previous search buttons is clicked 
             cityList.on("click", function (event) {
                 var btnText = $(event.target).text();
                 currentWeather(btnText);
                 fiveDayForecast(btnText);
             });
 
+            // Stores current list to local storage.
             localStorage.setItem("list", JSON.stringify(currentList));
         }
 
+        // Api to call for the current weather.
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&units=imperial&appid=9508f5887b64149ae87a4e8e95cc981b";
 
+        // calling on the api
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -62,11 +75,13 @@ $(document).ready(function () {
 
             var results = response;
 
+            // getting the latitude and logitude of the city search to get the uv index.
             var lat = results.coord.lat;
             var lon = results.coord.lon;
 
             var uv = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=9508f5887b64149ae87a4e8e95cc981b";
 
+            // calling to get the uv index for the current weather.
             $.ajax({
                 url: uv,
                 method: "GET"
@@ -78,8 +93,10 @@ $(document).ready(function () {
                 uvIndex.text("UV Index: " + res.value);
                 $("#results").append(uvIndex);
 
+                // Making the uv index an integer so it can be color coded with the css. 
                 var uvColor = parseInt(res.value);
 
+                // Checking what class to give the uv index value, so it can be color coded.
                 if (uvColor <= 2) {
                     uvIndex.attr("class", "green");
                 }
@@ -98,6 +115,7 @@ $(document).ready(function () {
 
             });
 
+            // The other api results for the current weather
             var wind = $("<p>");
             wind.text("Wind Speed: " + results.wind.speed + " mph");
             $("#results").prepend(wind);
@@ -110,6 +128,7 @@ $(document).ready(function () {
             temp.text("Temperature: " + results.main.temp + "\xB0 F");
             $("#results").prepend(temp);
 
+            // Gets the city name, current date, and weather icon.
             var city = $("<p>");
             city.html("<h2>" + results.name + moment().format((" (MMMM Do YYYY)") + "</h2>") + "<img src= http://openweathermap.org/img/w/" + results.weather[0].icon + ".png >");
             $("#results").prepend(city);
@@ -120,6 +139,7 @@ $(document).ready(function () {
 
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + City + "&units=imperial&appid=9508f5887b64149ae87a4e8e95cc981b";
 
+        // Api call to get the 5 day forecast.
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -127,6 +147,7 @@ $(document).ready(function () {
 
             var results = response;
 
+            // Gets the dates and same time for the next 5 days of weather from the api.
             var tomorrow = [
                 moment().add(1, 'days').format("YYYY-MM-DD") + " 15:00:00",
                 moment().add(2, 'days').format("YYYY-MM-DD") + " 15:00:00",
@@ -136,24 +157,24 @@ $(document).ready(function () {
             ];
             var k = 0;
 
+            // Reads the api response to get the weather for the date and time.
             for (let i = 0; i < results.list.length; i++) {
                 if (results.list[i].dt_txt === tomorrow[k]) {
                     k++;
 
+                    // Gets the date, weather icon, temperature at the time above, and humidity.
                     var dayX = $("<div>");
                     dayX.attr("class", "fivForecast");
                     dayX.html(moment().add(k, 'days').format("MMMM Do YYYY") + "<img src= http://openweathermap.org/img/w/" + results.list[i].weather[0].icon + ".png >" + "<p>Temperature: " + results.list[i].main.temp_max + "\xB0 F </p>" +
                         "<p>Humidity: " + results.list[i].main.humidity + "% </p>");
                     $("#forecast").append(dayX);
 
+                    // Stops the loop after getting the information 5 times.
                     if (k == 5) {
                         break;
                     }
                 }
             }
-
         });
-
     }
-
 });
